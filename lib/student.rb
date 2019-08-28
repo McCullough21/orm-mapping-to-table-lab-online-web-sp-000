d852236c53a683d2d577b0c7a5b09cf98ebedbfb
Michael McCullough <michael.mccullough21@gmail.com>
2019-08-28 14:16:30 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -23,9 +23,7 @@ def self.drop_table
   sql = <<-SQL
   DROP TABLE students
   SQL
-
-  DB[:conn].execute(sql)
-
+    DB[:conn].execute(sql)
 end
 
   # Remember, you can access your database connection anywhere in this class
